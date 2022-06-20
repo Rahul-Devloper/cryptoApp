3c5cd1121049101d14109828e0033c868539f0bf
Rahul-Devloper <rahulramesh26@yahoo.com>
2022-06-20 23:39:40 +0530

    fetched cryptocurrencies coins

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,6 +3,9 @@ import millify from "millify"
 import { Col, Row, Statistic, Typography } from "antd"
 
 import { useGetCryptosQuery } from "../services/cryptoApi"
+import { Link } from "react-router-dom"
+
+import { Cryptocurrencies, News } from "../components"
 
 const { Title } = Typography
 
@@ -48,6 +51,24 @@ const Homepage = () => {
           />
         </Col>
       </Row>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className='show-more'>
+          <Link to='/cryptocurrencies'>Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
+          Latesh Crypto News
+        </Title>
+        <Title level={3} className='show-more'>
+          <Link to='/news'>Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   )
 }
