3ab34b4068c95cc6e86127f3dd0b908c8757dfc1
Rahul-Devloper <rahulramesh26@yahoo.com>
2022-06-26 23:11:21 +0530

    implemented INput field for search

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import millify from "millify"
 import { Link } from "react-router-dom"
 import { Card, Row, Col, Input } from "antd"
@@ -6,17 +6,45 @@ import { Card, Row, Col, Input } from "antd"
 //fetching api
 import { useGetCryptosQuery } from "../services/cryptoApi"
 
-const Cryptocurrencies = () => {
-  const { data: cryptosList, isFetching } = useGetCryptosQuery()
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
+  const [cryptos, setCryptos] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  useEffect(() => {
+    // setCryptos(cryptosList?.data?.coins)
+
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
+    setCryptos(filteredData)
+  }, [cryptosList, searchTerm])
 
   console.log("cryptos=>", cryptos)
+  if (isFetching) return "Loading..."
+
   return (
     <>
-      <Row gutter={(32, 32)} className='crypto-card-container'>
-        {cryptos.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
+      {!simplified && (
+        <div className='search-crypto'>
+          <Input
+            placeholder='Search Cryptocurrency'
+            onChange={(e) => setSearchTerm(e?.target?.value)}
+          />
+        </div>
+      )}
+      <Row gutter={[32, 32]} className='crypto-card-container'>
+        {cryptos?.map((currency) => (
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            className='crypto-card'
+            key={currency.uuid}
+            style={{ margin: "0.25rem" }}>
+            <Link to={`/crypto/${currency.uuid}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
                 extra={<img className='crypto-image' src={currency.iconUrl} />}
